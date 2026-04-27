7a8bdf2606d4d17bded237269b8b778baf77453e
salvaharp-llc <salvaharp@gmail.com>
2026-04-27 13:11:05 +0200

    add augmented summary generation

--- cli/augmented_generation_cli.py
+++ cli/augmented_generation_cli.py
@@ -1,7 +1,8 @@
 import argparse
 import sys
 
-from lib.augmented_generation import rag_command
+from lib.augmented_generation import rag_command, summarize_command
+from lib.search_utils import DEFAULT_SEARCH_LIMIT
 
 def main():
     parser = argparse.ArgumentParser(description="Retrieval Augmented Generation CLI")
@@ -12,24 +13,33 @@ def main():
     )
     rag_parser.add_argument("query", type=str, help="Search query for RAG")
 
+    summarize_parser = subparsers.add_parser(
+        "summarize", help="Perform (search + document summarization)"
+    )
+    summarize_parser.add_argument("query", type=str, help="Search query for RAG")
+    summarize_parser.add_argument("--limit", type=int, default=DEFAULT_SEARCH_LIMIT, help="Search query for RAG")
+
     args = parser.parse_args()
 
     match args.command:
         case "rag":
-            result = rag_command(args.query)
-            
-            if "error" in result:
-                print(f"Error: {result["error"]}")
-                sys.exit()
-
-            print("Search Results:")
-            for search_result in result["search_results"]:
-                print(f"- {search_result["title"]}")
-            print()
-            print("RAG Response:")
-            print(result["answer"])
+            print_result(rag_command(args.query), "RAG Response")
+        case "summarize":
+            print_result(summarize_command(args.query, args.limit), "LLM Summary")
         case _:
             parser.print_help()
+
+def print_result(result: dict, answer_type: str) -> None:
+    if "error" in result:
+        print(f"Error: {result["error"]}")
+        sys.exit()
+
+    print("Search Results:")
+    for search_result in result["search_results"]:
+        print(f"- {search_result["title"]}")
+    print()
+    print(f"{answer_type}:")
+    print(result["answer"])
 
 if __name__ == "__main__":
     main()
--- cli/lib/augmented_generation.py
+++ cli/lib/augmented_generation.py
@@ -1,34 +1,64 @@
 from google import genai
 
 from .gen_utils import API_KEY, GEN_MODEL
-from .search_utils import load_movies
+from .search_utils import load_movies, DEFAULT_SEARCH_LIMIT
 from .hybrid_search import HybridSearch
 
+
 def rag_command(query: str) -> dict:
+    return generate_answer(query, "search")
+
+def summarize_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> dict:
+    return generate_answer(query, "summary", limit=limit)
+
+def format_search_results(results: list[dict]) -> str:
+    formatted_results = []
+    for result in results:
+        formatted_results.append(f"{result['title']}: {result['description']}")
+    return '\n\n'.join(formatted_results)
+
+def generate_answer(query: str, answer_type: str, limit: int = DEFAULT_SEARCH_LIMIT) -> dict:
     movies = load_movies()
     hs = HybridSearch(movies)
-    results = hs.rrf_search(query)
+    results = hs.rrf_search(query, limit=limit)
 
     if not results:
         return {
             "search_results": [],
             "error": "No results found",
         }
+    
+    match answer_type:
+        case "search":
+            prompt = f"""You are a RAG agent for Hoopla, a movie streaming service.
+            Your task is to provide a natural-language answer to the user's query based on documents retrieved during search.
+            Provide a comprehensive answer that addresses the user's query.
 
-    formatted_results = []
-    for result in results:
-        formatted_results.append(f"{result['title']}: {result['description']}")
+            Query: {query}
 
-    prompt = f"""You are a RAG agent for Hoopla, a movie streaming service.
-    Your task is to provide a natural-language answer to the user's query based on documents retrieved during search.
-    Provide a comprehensive answer that addresses the user's query.
+            Documents:
+            {format_search_results(results)}
 
-    Query: {query}
+            Answer:"""
+        case "summary":
+            prompt = f"""Provide information useful to the query below by synthesizing data from multiple search results in detail.
 
-    Documents:
-    {'\n\n'.join(formatted_results)}
+            The goal is to provide comprehensive information so that users know what their options are.
+            Your response should be information-dense and concise, with several key pieces of information about the genre, plot, etc. of each movie.
 
-    Answer:"""
+            This should be tailored to Hoopla users. Hoopla is a movie streaming service.
+
+            Query: {query}
+
+            Search results:
+            {format_search_results(results)}
+
+            Provide a comprehensive 3–4 sentence answer that combines information from multiple sources:"""
+        case _:
+            return {
+                "search_results": results,
+                "error": f"{answer_type} geneation is not supported",
+            }
 
     client = genai.Client(api_key=API_KEY)
     response = client.models.generate_content(model=GEN_MODEL, contents=prompt)
